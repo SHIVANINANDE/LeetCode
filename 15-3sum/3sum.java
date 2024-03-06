72d6ba7ffb62fe26a8fbebf92c419cbf9edcf40e
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-03-06 17:11:20 +0530

    Time: 33 ms (49.95%) | Memory: 51.5 MB (53.26%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,21 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-      List<List<Integer>> result = new ArrayList<>();
-        if(nums.length < 3) return result;
+       List<List<Integer>> res = new ArrayList<>();
+        
+        //sorting the initial array so that  brute forcing becomes easy
         Arrays.sort(nums);
-        int i = 0;
-        while(i < nums.length - 2) {
-            if(nums[i] > 0) break;
+        
+        for (int i = 0; i < nums.length; ++i) {
+            
+            // Never let i refer to the same value twice to avoid duplicates.
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
+            
             int j = i + 1;
             int k = nums.length - 1;
-            while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0) result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                if(sum <= 0) while(nums[j] == nums[++j] && j < k);
-                if(sum >= 0) while(nums[k--] == nums[k] && j < k);
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));  //push the arrays in result..
+                    ++j;
+                    // Never let j refer to the same value twice (in an output) to avoid duplicates..
+                    while (j < k && nums[j] == nums[j-1])
+                        ++j;
+                }
+                
+                //since array is sorted and sum is less than 0 then it means that the valid sum could be present later in the array so we increase the forward  pointer...
+                else if (nums[i] + nums[j] + nums[k] < 0) {
+                    ++j;
+                }
+                
+                //since array is sorted and sum is greater than 0 then it means that the valid sum could be present earlier in the array so we decrease the backward pointer...
+                else {
+                    --k;
+                }
             }
-            while(nums[i] == nums[++i] && i < nums.length - 2);
         }
-        return result;  
+        return res;    //retuirn the result.. 
     }
 }
