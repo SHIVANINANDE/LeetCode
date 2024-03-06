1a14e9354533a8178b00cc8f84b3b77317357001
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-03-06 15:42:16 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (12.02%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,26 +1,34 @@
+
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n = intervals.length; 
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            if(ans.isEmpty() || intervals[i][0]>ans.get(ans.size()-1).get(1)){
-                ans.add(Arrays.asList(intervals[i][0], intervals[i][1]));
-            }else{
-                ans.get(ans.size() - 1).set(1,Math.max(ans.get(ans.size() - 1).get(1), intervals[i][1]));
-            }
-        }
-
-        int[][] result = new int[ans.size()][2];
-        for (int i = 0; i < ans.size(); i++) {
-            result[i][0] = ans.get(i).get(0);
-            result[i][1] = ans.get(i).get(1);
-        }
-
-        return result;
+        int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < intervals.length; i++) {
+			min = Math.min(min, intervals[i][0]);
+			max = Math.max(max, intervals[i][0]);
+		}
+		
+		int[] range = new int[max - min + 1];
+		for (int i = 0; i < intervals.length; i++) {
+			range[intervals[i][0] - min] = Math.max(intervals[i][1] - min, range[intervals[i][0] - min]); 
+		}
+		
+		int start = 0, end = 0;
+		LinkedList<int[]> result = new LinkedList<>();
+		for (int i = 0; i < range.length; i++) {
+			if (range[i] == 0) {
+				continue;
+			}
+			if (i <= end) {
+				end = Math.max(range[i], end);
+			} else {
+				result.add(new int[] {start + min, end + min});
+				start = i;
+				end = range[i];
+			}
+		}
+		result.add(new int[] {start + min, end + min});
+		return result.toArray(new int[result.size()][]);
     }
 }
