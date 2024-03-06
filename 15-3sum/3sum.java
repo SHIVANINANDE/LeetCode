dd62e118bba40fd7bb61161afa6f603e8e9695d3
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-03-06 17:12:48 +0530

    Time: 434 ms (26.05%) | Memory: 53.9 MB (5.54%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,38 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       List<List<Integer>> res = new ArrayList<>();
-        
-        //sorting the initial array so that  brute forcing becomes easy
-        Arrays.sort(nums);
-        
-        for (int i = 0; i < nums.length; ++i) {
-            
-            // Never let i refer to the same value twice to avoid duplicates.
-            if (i != 0 && nums[i] == nums[i - 1])
-                continue;
-            
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                if (nums[i] + nums[j] + nums[k] == 0) {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));  //push the arrays in result..
-                    ++j;
-                    // Never let j refer to the same value twice (in an output) to avoid duplicates..
-                    while (j < k && nums[j] == nums[j-1])
-                        ++j;
+       Set<List<Integer>> resultSet = new HashSet();
+
+        Set<Integer> duplicatedSet = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length - 2; i++) {
+            if (!duplicatedSet.add(nums[i])) continue;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                int value = 0 - nums[i] - nums[j];
+                if (map.containsKey(value) && map.get(value) == i) {
+                    List<Integer> list = new ArrayList<>(Arrays.asList(nums[i], nums[j], value));
+                    Collections.sort(list);
+                    resultSet.add(list);
                 }
-                
-                //since array is sorted and sum is less than 0 then it means that the valid sum could be present later in the array so we increase the forward  pointer...
-                else if (nums[i] + nums[j] + nums[k] < 0) {
-                    ++j;
-                }
-                
-                //since array is sorted and sum is greater than 0 then it means that the valid sum could be present earlier in the array so we decrease the backward pointer...
-                else {
-                    --k;
-                }
+                map.put(nums[j], i);
             }
         }
-        return res;    //retuirn the result.. 
+        return new ArrayList<>(resultSet) ; 
     }
 }
