d6a1bde5889e51742c1e61ace3c312213e467e9a
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-03-06 20:04:18 +0530

    Time: 22 ms (99.10%) | Memory: 93.2 MB (37.76%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,46 +1,47 @@
 class RandomizedSet {
 
-    List<Integer> nums;
-    Map<Integer, Integer> idxMap;
-    Random random;
+   private Map<Integer, Integer> valueToIndexMap;
+    private List<Integer> values;
+    private Random rand;
 
-    public RandomizedSet() {
-        nums = new ArrayList<>();
-        idxMap = new HashMap<>();
-        random = new Random();
-    }
+    public RandomizedSet() { // --------------------------------/
+        valueToIndexMap = new HashMap<>(); //O(1) average time  /
+        values = new ArrayList<>(); //O(1) average time         / -- O(1)
+        rand = new Random(); //O(1) average time                /
+    } //--------------------------------------------------------/
+    
+    public boolean insert(int val) { //---------------------------/
+        if (valueToIndexMap.containsKey(val)) { //O(1) Average    /
+            return false;                                        //
+        }                                                        // -- O(1) 
+        valueToIndexMap.put(val, values.size()); //O(1) Average   /
+        values.add(val); //O(1) Average                           /
+        return true;                                             // 
+    } //----------------------------------------------------------/
+    
+    public boolean remove(int val) { // ----------------------------/
+        if (!valueToIndexMap.containsKey(val)) { //O(1) Average     /
+            return false;                                          //
+        }                                                          //
+        int index = valueToIndexMap.get(val);  //O(1) Average       /                   
+        int lastElement = values.get(values.size() - 1);//O(1) Avg  /
+                                                                   //
+        values.set(index, lastElement);   //O(1) Average            /
+        // Move the last element to the 'deleted' position          /  --- O(1) 
+        valueToIndexMap.put(lastElement, index); //O(1) Average     /
+        // Update the index of the last element in the map          /
+                                                                   //             
+        values.remove(values.size() - 1); //O(1) Average            / 
+        // Remove the last element                                  /
+        valueToIndexMap.remove(val);  //O(1) Average                /
+        // Remove the deleted element from map                      /   
+        return true;                                               //
+    } //------------------------------------------------------------/
+    
+    public int getRandom() { //---------------------------------------/
+        int randomIndex = rand.nextInt(values.size()); //O(1) Average /
+        return values.get(randomIndex);//O(1) Average
 
-    public boolean insert(int val) {
-        if (idxMap.containsKey(val)) {
-            return false;
-        }
-
-        idxMap.put(val, nums.size());
-        nums.add(val);
-        return true;
-    }
-
-    public boolean remove(int val) {
-        if (!idxMap.containsKey(val)) {
-            return false;
-        }
-
-        int idx = idxMap.get(val);
-        int lastIdx = nums.size() - 1;
-        if (idx != lastIdx) {
-            int lastVal = nums.get(lastIdx);
-            nums.set(idx, lastVal);
-            idxMap.put(lastVal, idx);
-        }
-        nums.remove(lastIdx);
-        idxMap.remove(val);
-        return true;
-    }
-
-    public int getRandom() {
-        return nums.get(random.nextInt(nums.size()));
-    }
-}
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
@@ -49,3 +50,5 @@ class RandomizedSet {
  * boolean param_2 = obj.remove(val);
  * int param_3 = obj.getRandom();
  */
+    }
+}
