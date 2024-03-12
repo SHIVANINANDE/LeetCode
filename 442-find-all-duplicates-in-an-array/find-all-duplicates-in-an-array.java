3572f50c4a0bce4025fc34dfd94e2569b191e3d6
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-03-12 09:22:01 +0530

    Time: 6 ms (74.03%) | Memory: 53.5 MB (66.30%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,17 +1,14 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> res = new ArrayList<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            
-            int idx = Math.abs(nums[i]) - 1; // Get the mapped index for nums[i], using Math.abs only once
-            
-            if (nums[idx] < 0) // Check if nums[i] has been seen before, if true then add to the output list
-                res.add(idx + 1); // Need to add +1 as our range is 1-based
-            
-            nums[idx] *= -1; // Unconditionally flip the sign, if it's positive, we want to mark it as negative. But if it's negative, we just recover previous value so that at the end our array is not modified
+        final List<Integer> duplicates = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            final int n = Math.abs(nums[i]);
+            if(nums[n - 1] < 0) {
+                duplicates.add(n);
+            } else {
+                nums[n - 1] *= -1;
+            }
         }
-        
-        return res;
+        return duplicates;
     }
 }
