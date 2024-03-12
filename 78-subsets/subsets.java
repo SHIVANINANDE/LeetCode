b88d506a2a8dd7993cc37a17d3a87e857e6dd74b
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-03-12 12:01:27 +0530

    Time: 1 ms (62.91%) | Memory: 42.6 MB (67.31%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,16 +1,26 @@
 class Solution {
+     List<List<Integer>> result;
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, 0);
-    return list;
+        result = new ArrayList();
+        if(nums==null || nums.length==0) return result;
+        
+        subsets(nums,new ArrayList<Integer>(), 0);
+        return result;
     }
-    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-    list.add(new ArrayList<>(tempList));
-    for(int i = start; i < nums.length; i++){
-        tempList.add(nums[i]);
-        backtrack(list, tempList, nums, i + 1);
-        tempList.remove(tempList.size() - 1);
+   private void subsets(int[] nums, ArrayList<Integer> temp, int index) {
+        // base condition
+        if(index >= nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        // main logic
+		// case 1 : we pick the element
+        temp.add(nums[index]);
+        subsets(nums, temp, index+1); // move ahead
+        temp.remove(temp.size()-1);
+        
+		// case 2 : we don't pick the element ( notice, we did not add the current element in our temporary list
+        subsets(nums, temp, index+1); // move ahead
     }
-}
 }
