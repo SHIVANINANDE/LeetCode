6fab19f8172440b8edf323d79ef4e884d7532035
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-04-05 14:23:19 +0530

    Time: 131 ms (82.28%) | Memory: 41.4 MB (57.66%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -4,7 +4,7 @@ class Solution {
         visited = new boolean[board.length][board[0].length];
         
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
+            for(int j = 0; j < board[0].length; j++){
                 if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
                     return true;
                 }
