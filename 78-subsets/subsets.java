12a2cc9171f995e6becee8bb570f0a8d352aec03
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-04-11 23:55:14 +0530

    Time: 1 ms (60.77%) | Memory: 42.5 MB (61.97%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -7,11 +7,14 @@ class Solution {
         return list;
     }
    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-    list.add(new ArrayList<>(tempList));
-    for(int i = start; i < nums.length; i++){
-        tempList.add(nums[i]);
-        backtrack(list, tempList, nums, i + 1);
+    if (start == nums.length) {
+            list.add(new ArrayList<>(tempList));
+            return;
+        }
+        
+        tempList.add(nums[start]);
+        backtrack(list, tempList, nums, start + 1);
         tempList.remove(tempList.size() - 1);
-    }
+        backtrack(list, tempList, nums, start + 1);
     }
 }
