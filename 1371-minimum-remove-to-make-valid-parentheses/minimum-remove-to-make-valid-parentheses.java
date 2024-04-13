09ebbd7a52bf87cdb59a4e2b010635247a7380dd
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-04-13 22:18:48 +0530

    Time: 31 ms (25.16%) | Memory: 45.7 MB (18.30%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,46 +1,24 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        int leftCount = 0;
-        int rightCount = 0;
-
-        // Use a stack to keep track of valid parentheses
-        Stack<Character> stack = new Stack<>();
-
-        // Pass 1: Iterate through the string and process parentheses
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-
-            // Increment count for left parentheses
-            if (currentChar == '(') {
-                leftCount++;
-            }
-            // Increment count for right parentheses
-            if (currentChar == ')') {
-                rightCount++;
-            }
-
-            // If there are more right parentheses than left, skip the current right parenthesis
-            if (rightCount > leftCount) {
-                rightCount--; // Decrease right count
-                continue;     // Skip processing this right parenthesis
-            } else {
-                stack.push(currentChar); // Add valid parentheses to the stack
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            if(Character.isAlphabetic(ch))
+                continue;
+            if(ch == '(')
+                stack.push(i);
+            else {
+                if(!stack.isEmpty() && s.charAt(stack.peek()) == '(')
+                    stack.pop();
+                else stack.push(i);
             }
         }
         StringBuilder result = new StringBuilder();
-        while (!stack.isEmpty()) {
-            char currentChar = stack.pop();
-            // If there are more left parentheses than right, skip the current left parenthesis
-            if (leftCount > rightCount && currentChar == '(') {
-                leftCount--; // Decrease left count
-                // Do nothing, skip this left parenthesis
-            } else {
-                result.append(currentChar); // Add valid parentheses to the result
-            }
-        }
-
-        // Reverse the result string and return
-        return result.reverse().toString();
-    
+        HashSet<Integer> set = new HashSet<>(stack);
+        for(int i=0;i<s.length();i++)
+            if(!set.contains(i))
+                result.append(s.charAt(i));
+        
+        return result.toString();
     }
 }
