96e2ba3e62c2d283c7252d6eeafb209ef95dfc9c
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-04-22 14:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (74.25%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -5,17 +5,17 @@ class Solution {
         fill(image, sr, sc, color, image[sr][sc]);
         return image;
     }
-     public void fill(int[][] image, int sr, int sc, int color, int cur) {
+     public void fill(int[][] image, int sr, int sc, int color, int newClr) {
     
         if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length) return;
         
-        if(cur != image[sr][sc]) return;
+         if(newClr != image[sr][sc]) return;
        
         image[sr][sc] = color;
        
-        fill(image, sr-1, sc, color, cur);
-        fill(image, sr+1, sc, color, cur);
-        fill(image, sr, sc-1, color, cur);
-        fill(image, sr, sc+1, color, cur);
+        fill(image, sr-1, sc, color, newClr);
+        fill(image, sr+1, sc, color, newClr);
+        fill(image, sr, sc-1, color, newClr);
+        fill(image, sr, sc+1, color, newClr);
     }
 }
