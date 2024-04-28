4e5123886efb02200ef1390bfa7be67ca1d21bab
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-04-28 22:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (99.01%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,12 +1,12 @@
 class Solution {
     public int longestSubstring(String s, int k) {
         if (s == null || s.length() == 0) return 0;
-        char[] chars = new char[26];
+        char[] freq = new char[26];
         // record the frequency of each character
-        for (int i = 0; i < s.length(); i += 1) chars[s.charAt(i) - 'a'] += 1;
+        for (int i = 0; i < s.length(); i += 1) freq[s.charAt(i) - 'a'] += 1;
         boolean flag = true;
-        for (int i = 0; i < chars.length; i += 1) {
-            if (chars[i] < k && chars[i] > 0) flag = false;
+        for (int i = 0; i < freq.length; i += 1) {
+            if (freq[i] < k && freq[i] > 0) flag = false;
         }
         // return the length of string if this string is a valid string
         if (flag == true) return s.length();
@@ -14,7 +14,7 @@ class Solution {
         int start = 0, cur = 0;
         // otherwise we use all the infrequent elements as splits
         while (cur < s.length()) {
-            if (chars[s.charAt(cur) - 'a'] < k) {
+            if (freq[s.charAt(cur) - 'a'] < k) {
                 result = Math.max(result, longestSubstring(s.substring(start, cur), k));
                 start = cur + 1;
             }
