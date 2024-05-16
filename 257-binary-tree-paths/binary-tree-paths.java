b3b5651e1a21641d24376eccf79ca9e9a6770140
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-05-16 08:55:18 +0530

    Time: 7 ms (60.29%) | Memory: 42.8 MB (18.46%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -15,13 +15,19 @@
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> answer = new ArrayList<String>();
-        if (root != null) searchBT(root, "", answer);
-        return answer;
+         List<String> sList=new LinkedList<String>();
+    //String s=new String();
+    if (root==null) return sList;
+    if (root.left==null && root.right==null) {
+        sList.add(Integer.toString(root.val));
+        return sList;
     }
-    private void searchBT(TreeNode root, String path, List<String> answer) {
-    if (root.left == null && root.right == null) answer.add(path + root.val);
-    if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
-    if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
-}
-}
+    
+    for (String s: binaryTreePaths(root.left)) {
+        sList.add(Integer.toString(root.val)+"->"+s);
+    }
+    for (String s: binaryTreePaths(root.right)) {
+        sList.add(Integer.toString(root.val)+"->"+s);
+    }
+    return sList;
+}}
