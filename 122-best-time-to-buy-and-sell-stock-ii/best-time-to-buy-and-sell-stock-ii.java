86939eafcd94771f9a8b5ac057f1e5acc2881f1b
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2024-10-25 16:42:08 +0530

    Time: 4 ms (6.47%) | Memory: 49.8 MB (5.09%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,30 +1,25 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int n=prices.length;
-       int[] ahead = new int[2];
-        int[] cur = new int[2];
-
-        // Base condition: If we have no stocks to buy or sell, profit is 0
-        ahead[0] = ahead[1] = 0;
-
-        int profit = 0;
-
-        // Iterate through the array in reverse to calculate the maximum profit
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                if (buy == 0) { // We can buy the stock
-                    profit = Math.max(0 + ahead[0], -prices[ind] + ahead[1]);
-                }
-
-                if (buy == 1) { // We can sell the stock
-                    profit = Math.max(0 + ahead[1], prices[ind] + ahead[0]);
-                }
-                cur[buy] = profit;
-            }
-
-            // Update the 'ahead' array with the current profit values
-            System.arraycopy(cur, 0, ahead, 0, 2);
+    public int solve(int idx, int buy, int n, int[][] dp, int[] ar){
+        if(idx==n)return 0;
+        if(dp[idx][buy]!=-1){
+            return dp[idx][buy];
         }
-        return cur[0]; // The maximum profit is stored in 'cur[0]'
+        int profit=0;
+        if(buy==0){
+            profit=Math.max((solve(idx+1, 0,n,dp,ar)),(-ar[idx]+solve(idx+1, 1,n,dp,ar)));
+        }
+        if(buy==1){
+            profit=Math.max((solve(idx+1, 1,n,dp,ar)),(ar[idx]+solve(idx+1, 0,n,dp,ar)));
+        }
+        return dp[idx][buy]=profit;
+    }
+    public int maxProfit(int[] prices) {
+       int n = prices.length; 
+       int[][] dp=new int[n][2];
+       for(int[] row:dp){
+        Arrays.fill(row,-1);
+       }
+       int ans = solve(0,0,n,dp,prices);
+       return ans;
     }
 }
