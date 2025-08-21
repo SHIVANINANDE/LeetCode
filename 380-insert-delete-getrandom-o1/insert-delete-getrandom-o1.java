a463bd787e4bd828e36903d7b1f47babf7786a89
Shivani Nande <51069039+SHIVANINANDE@users.noreply.github.com>
2025-08-21 16:39:10 +0530

    Time: 32 ms (36.69%) | Memory: 88.3 MB (95.62%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,47 +1,42 @@
 class RandomizedSet {
-
-   private Map<Integer, Integer> valueToIndexMap;
-    private List<Integer> values;
-    private Random randomNum;
-
-    public RandomizedSet() { // --------------------------------/
-        valueToIndexMap = new HashMap<>(); //O(1) average time  /
-        values = new ArrayList<>(); //O(1) average time         / -- O(1)
-        randomNum = new Random(); //O(1) average time                /
-    } //--------------------------------------------------------/
+    ArrayList<Integer> list;
+    HashMap<Integer, Integer> mpp;
     
-    public boolean insert(int val) { //---------------------------/
-        if (valueToIndexMap.containsKey(val)) { //O(1) Average    /
-            return false;                                        //
-        }                                                        // -- O(1) 
-        valueToIndexMap.put(val, values.size()); //O(1) Average   /
-        values.add(val); //O(1) Average                           /
-        return true;                                             // 
-    } //----------------------------------------------------------/
-    
-    public boolean remove(int val) { // ----------------------------/
-        if (!valueToIndexMap.containsKey(val)) { //O(1) Average     /
-            return false;                                          //
-        }                                                          //
-        int index = valueToIndexMap.get(val);  //O(1) Average       /                   
-        int lastElement = values.get(values.size() - 1);//O(1) Avg  /
-                                                                   //
-        values.set(index, lastElement);   //O(1) Average            /
-        // Move the last element to the 'deleted' position          /  --- O(1) 
-        valueToIndexMap.put(lastElement, index); //O(1) Average     /
-        // Update the index of the last element in the map          /
-                                                                   //             
-        values.remove(values.size() - 1); //O(1) Average            / 
-        // Remove the last element                                  /
-        valueToIndexMap.remove(val);  //O(1) Average                /
-        // Remove the deleted element from map                      /   
-        return true;                                               //
-    } //------------------------------------------------------------/
-    
-    public int getRandom() { //---------------------------------------/
-        int randomIndex = randomNum.nextInt(values.size()); //O(1) Average /
-        return values.get(randomIndex);//O(1) Average
 
+    public RandomizedSet() {
+        this.list=new ArrayList<>();
+        this.mpp= new HashMap<>();
+    }
+    
+    public boolean insert(int val) {
+        if(mpp.containsKey(val)){
+            return false;
+        }
+        list.add(val);
+        mpp.put(val, list.size()-1);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if(!mpp.containsKey(val)){
+            return false;
+        }
+        int idx=mpp.get(val);
+        mpp.put(list.get(list.size()-1),idx);
+        int temp=list.get(idx);
+        list.set(idx,list.get(list.size()-1));
+        list.set(list.size()-1, temp);
+        list.remove(list.size()-1);
+        mpp.remove(val);
+        return true;
+        
+    }
+    
+    public int getRandom() {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+}
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
@@ -50,5 +45,3 @@ class RandomizedSet {
  * boolean param_2 = obj.remove(val);
  * int param_3 = obj.getRandom();
  */
-    }
-}
