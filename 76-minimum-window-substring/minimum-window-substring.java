1eb55352abe7342291f6ec9f0a23ccaf5d3d7fd9
Shivani Nande <shivani.golu.nande.8@gmail.com>
2026-04-30 08:55:44 +0530

    Time: 16 ms (60.68%) | Memory: 47.2 MB (50.73%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,35 +1,41 @@
+import java.util.*;
+
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
-                s.length() < t.length()) {
-            return new String();
-        }
-        int[] map = new int[128];
-        int count = t.length();
-        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
-        /// UPVOTE !
+        Map<Character, Integer> map = new HashMap<>();
+
         for (char c : t.toCharArray()) {
-            map[c]++;
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        char[] chS = s.toCharArray();
+        int l = 0, count = t.length();
+        int minLen = Integer.MAX_VALUE, start = 0;
 
-        while (end < chS.length) {
-            if (map[chS[end++]]-- > 0) {
-                count--;
+        for (int r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+
+            if (map.containsKey(c)) {
+                if (map.get(c) > 0) count--;
+                map.put(c, map.get(c) - 1);
             }
+
             while (count == 0) {
-                if (end - start < minLen) {
-                    startIndex = start;
-                    minLen = end - start;
+                if (r - l + 1 < minLen) {
+                    minLen = r - l + 1;
+                    start = l;
                 }
-                if (map[chS[start++]]++ == 0) {
-                    count++;
+
+                char left = s.charAt(l);
+
+                if (map.containsKey(left)) {
+                    map.put(left, map.get(left) + 1);
+                    if (map.get(left) > 0) count++;
                 }
+
+                l++;
             }
         }
 
-        return minLen == Integer.MAX_VALUE ? new String() :
-                new String(chS, startIndex, minLen);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
